600eb6483e8c9e962a656e2acd440a8f42645e33
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-26 03:30:15 +0200

    Serve multiple static files

---
+++ JavaScript/3-server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+
+const cache = new Map();
+
+const cacheFile = (path) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      cache.delete(path);
+      return;
+    }
+    cache.set(path, data);
+  });
+};
+
+const cacheFolder = (path) => {
+  fs.readdir(path, (err, files) => {
+    if (err) return;
+    files.forEach(cacheFile);
+  });
+};
+
+const watch = (path) => {
+  fs.watch(path, (event, file) => {
+    cacheFile(file);
+  });
+};
+
+const path = './';
+cacheFolder(path);
+watch(path);
+
+http.createServer((req, res) => {
+  const url = req.url.substring(1);
+  console.log(url);
+  const data = cache.get(url);
+  if (data) res.end(data);
+  else res.end('File ' + url + 'not found');
+}).listen(8000);
